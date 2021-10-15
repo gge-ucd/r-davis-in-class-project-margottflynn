8beeadb7421c596f7699837024358cd14faef316
Margot Flynn <margotflynn@Margots-MacBook-Pro.local>
2021-10-15 09:11:53 -0700

    Week 4 in class work

---
+++ scripts/week_4.R
@@ -0,0 +1,12 @@
+#in-class challenge questions
+#select only species id, add new column called hindfoot_half containing values that are half the hindfoot_length
+#in this new column there are no NAs and all values are less than 30
+
+surveys_hindfoot_half <- surveys %>% filter(!is.na(hindfoot_length)) %>%
+  mutate(hindfoot_half = hindfoot_length/2) %>%
+  select(species_id, hindfoot_half) %>%
+  filter(hindfoot_half <30)
+surveys_hindfoot_half
+
+#group by and summarize, use to find the mean, min, and max hindfoot length for each species (using species_id)
+surveys %>% group_by()

fd037cbf04422049d2850ebbd954fccc3a208c5b
margottflynn <margotflynn@Margots-MacBook-Pro.local>
2021-10-19 16:13:13 -0700

    Week 4 homework and classwork

--- scripts/week_4.R
+++ scripts/week_4.R
@@ -9,4 +9,9 @@ surveys_hindfoot_half <- surveys %>% filter(!is.na(hindfoot_length)) %>%
 surveys_hindfoot_half
 
 #group by and summarize, use to find the mean, min, and max hindfoot length for each species (using species_id)
-surveys %>% group_by()
+surveys_summary_stats <- surveys %>%
+  filter(!is.na(hindfoot_length)) %>%
+group_by(species_id) %>%
+  summarise(mean(hindfoot_length), min(hindfoot_length), max(hindfoot_length))
+surveys_summary_stats
+
---
+++ scripts/week_4_homework.R
@@ -0,0 +1,7 @@
+#add tidyverse
+library(tidyverse)
+
+#create "surveys" tibble from existing data
+surveys <- read_csv("data/portal_data_joined.csv")
+
+read_csv("r-davis-in-class-project-margottflynn/data/portal_data_joined.csv")
